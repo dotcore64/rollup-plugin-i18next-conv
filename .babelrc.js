5595b5b83e93e4e68be5f9a2421224c597dd6354
Julian Grinblat <julian@dotcore.co.il>
2018-10-25 00:20:10 +0900

    Upgrade babel 7

--- .babelrc.js
+++ .babelrc.js
@@ -3,8 +3,5 @@ const targets = env === 'test' ? { node: 'current' } : { node: '6' };
 
 module.exports = {
   presets: [['@babel/env', { targets }]],
-  plugins: [
-    '@babel/proposal-object-rest-spread',
-    ...(env === 'test') ? ['istanbul'] : [],
-  ],
+  plugins: (env === 'test') ? ['istanbul'] : [],
 };
