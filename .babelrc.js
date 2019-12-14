efbbf20ffac02cc893f8072d638499be1ad16925
Julian Grinblat <julian@dotcore.co.il>
2019-12-14 20:25:13 +0900

    Drop support for node < 8, add support for node 12

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,7 @@
 module.exports = api => ({
   presets: [
     ['@babel/env', {
-      targets: api.env() === 'test' ? { node: 'current' } : { node: '6' },
+      targets: api.env() === 'test' ? { node: 'current' } : { node: '8' },
     }]
   ],
   plugins: (api.env() === 'test') ? ['istanbul'] : [],
